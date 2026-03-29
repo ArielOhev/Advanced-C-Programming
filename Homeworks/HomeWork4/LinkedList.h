12e2584fb9630722841a0afe738b91e9de2730b1
Ariel Ohev <ohevariel@gmail.com>
2026-03-29 20:26:13 +0300

    sync

--- Homeworks/HomeWork4/LinkedList.h
+++ Homeworks/HomeWork4/LinkedList.h
@@ -1,10 +1,9 @@
 #ifndef LIST_H
 #define LIST_H
 
-// הגדרת המבנה עבור איבר ברשימה מקושרת
 typedef struct item {
     int data;
-    struct item* next; // כאן חייב להיות רשום item (או השם שנתת אחרי ה-struct)
+    struct item* next; 
 } item;
 
 #endif#pragma once
